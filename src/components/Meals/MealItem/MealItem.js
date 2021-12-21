6a068971021751c4798fb37ba7914d6da4ef2527
Aslam Parvej <aslamparvej2@gmail.com>
2021-12-22 00:23:25 +0530

    add mealItem and Card

---
+++ src/components/Meals/MealItem/MealItem.js
@@ -0,0 +1,18 @@
+import React from "react";
+import classes from "./MealItem.module.css";
+
+const MealItem = (props) => {
+    const price = `$${props.price.toFixed(2)}`
+    return (
+        <li className={classes.meal}>
+            <div>
+                <h1>{props.title}</h1>
+                <div className={classes.description}>{props.description}</div>
+                <div className={classes.price}>{price}</div>
+            </div>
+            <div>
+            </div>
+        </li>
+    );
+}
+export default MealItem;
