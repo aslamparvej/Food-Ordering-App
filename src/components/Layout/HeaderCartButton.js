7d16382982a5fe30674480e3ceb452346d1f222b
Aslam Parvej <aslamparvej2@gmail.com>
2021-12-21 22:28:48 +0530

    Add existing file

---
+++ src/components/Layout/HeaderCartButton.js
@@ -0,0 +1,14 @@
+import React from "react";
+import classes from "./HeaderCartButton.module.css";
+import CartItem from "../Cart/CartIcon";
+
+const HeaderCartButton = (props)=>{
+    return <button className={classes.button}>
+        <span className={classes.icon}>
+            <CartItem />
+        </span>
+        <span>Your Cart</span>
+        <span className={classes.badge}>3</span>
+    </button>
+};
+export default HeaderCartButton;
